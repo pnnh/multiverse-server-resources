e43695e09da7e5a24ad58ae6ae7e71a518fecfa8
Larry <xspanni@gmail.com>
2022-12-09 10:02:44 +0000

    跑通一个EF Core读取数据的示例

--- Gliese/Models/Model.cs
+++ Gliese/Models/Model.cs
@@ -1,6 +1,33 @@
 namespace Gliese.Models;
 
-public class Model
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class BloggingContext : DbContext
 {
-    
+    public DbSet<Articles> Articles { get; set; }
+
+    public BloggingContext()
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // string sPath = Environment.GetEnvironmentVariable("CSHARP_DSN"); 
+
+            options.UseNpgsql(@"XXXXX");
+
+        }
+}
+
+ [Table("articles")]
+[PrimaryKey(nameof(Pk))]
+public class Articles
+{ 
+    [Column("pk")]
+    public string Pk { get; set; } = "";
+    [Column("title")]
+    public string Title { get; set; } = "";
 }
