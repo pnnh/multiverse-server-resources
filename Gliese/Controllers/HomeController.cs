92f23fb4153e17e884cdbdd33517f1bc64710b7b
Larry <xspanni@gmail.com>
2022-12-18 06:37:05 +0000

    简单实现在首页显示页码

--- Gliese/Controllers/HomeController.cs
+++ Gliese/Controllers/HomeController.cs
@@ -16,18 +16,48 @@ public class HomeController : Controller
         dataContext = configuration;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int page = 1)
     {
         var fBlogs = dataContext.Articles.Where(b => b.Title != "").ToList();
-        var model = new IndexViewModel {
+        var articlesCount = dataContext.Articles.Count();
+
+
+        var indexPageSize = 8;
+        var currentPage = page;
+        var maxPage = articlesCount / indexPageSize;
+        if (articlesCount % indexPageSize != 0)
+        {
+            maxPage += 1;
+        }
+        if (currentPage > maxPage)
+        {
+            currentPage = maxPage;
+        }
+        int startPage = currentPage - 5;
+        int endPage = currentPage + 5;
+        if (startPage < 1)
+        {
+            startPage = 1;
+        }
+        if (endPage > maxPage)
+        {
+            endPage = maxPage;
+        }
+        int prevPage = currentPage - 1;
+        int nextPage = currentPage + 1;
+
+        var model = new IndexViewModel
+        {
             Range = fBlogs,
+            Count = articlesCount,
+            CurrentPage = page,
+            StartPage = startPage,
+            EndPage = endPage,
+            PrevPage = prevPage,
+            NextPage = nextPage,
+            MaxPage = maxPage,
         };
         return View(model);
-    }
-  
-    public IActionResult Privacy()
-    {
-        return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
